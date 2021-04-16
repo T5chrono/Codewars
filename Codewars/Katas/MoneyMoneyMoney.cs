8f54ce20dbc12be48ed126b1931fa55f0ea142f9
Tomek Giela <tomasz.giela@hotmail.com>
2021-04-16 12:36:30 +0200

    Add MoneyMonyMoney Kata

---
+++ Codewars/Katas/MoneyMoneyMoney.cs
@@ -0,0 +1,29 @@
+﻿namespace Codewars.Katas
+{
+    public class MoneyMoneyMoney
+    {
+        public static int CalculateYears(double principal, double interest, double tax, double desiredPrincipal)
+        {
+            if (principal == desiredPrincipal) return 0;
+
+            int years = 0;
+            double currentPrincipal = principal;
+
+            while (currentPrincipal < desiredPrincipal)
+            {
+                currentPrincipal = CalculateOneYearPrincipal(currentPrincipal, interest, tax);
+                years += 1;
+            }
+
+            return years;
+        }
+
+        public static double CalculateOneYearPrincipal(double principal, double interest, double tax)
+        {
+            var untaxedInterst = principal * interest;
+            var taxToPay = untaxedInterst * tax;
+
+            return principal + untaxedInterst - taxToPay;
+        }
+    }
+}
---
+++ XUnitKatasTests/MoneyMoneyMoneyShould.cs
@@ -0,0 +1,40 @@
+﻿using AutoFixture;
+using Codewars.Katas;
+using Xunit;
+
+namespace XUnitKatasTests
+{
+    public class MoneyMoneyMoneyShould
+    {
+        private readonly Fixture _fixture;
+
+        public MoneyMoneyMoneyShould()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void ReturnZeroWhenPrincipalAndDesiredPrincipalAreEqual()
+        {
+            var someNumber = _fixture.Create<double>();
+
+            Assert.Equal(0, MoneyMoneyMoney.CalculateYears(someNumber, someNumber, someNumber, someNumber));
+        }
+
+        [Theory]
+        [InlineData(100, 0.2, 0.5, 110, 1)]
+        [InlineData(100, 0.2, 0.5, 111, 2)]
+        public void ReturnProperAmountOfYearsToSaveDesiredPrincipal(double principal, double interest, double tax, double desiredPrincipal, int expectedYears)
+        {
+            Assert.Equal(expectedYears, MoneyMoneyMoney.CalculateYears(principal, interest, tax, desiredPrincipal));
+        }
+
+        [Theory]
+        [InlineData(100, 0.2, 0.5, 110)]
+        [InlineData(100, 0.05, 0.18, 104.1)]
+        public void ReturnProperPrincipalAfterOneYear(double principal, double interest, double tax, double expectedPrincipal)
+        {
+            Assert.Equal(expectedPrincipal, MoneyMoneyMoney.CalculateOneYearPrincipal(principal, interest, tax));
+        }
+    }
+}
