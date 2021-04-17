3bff2b998494a739635a8196b044cefe9d22f4ce
Tomek Giela <tomasz.giela@hotmail.com>
2021-04-17 11:41:12 +0200

    Add MonkeyMath01 Kata

---
+++ Codewars/Katas/MonkeysMath01.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Codewars.Katas
+{
+    class MonkeysMath01
+    {
+        public static readonly char[] singleZeros = "abdegopq069DOPQR".ToCharArray();
+        public static readonly char[] doubleZeros = "%&B8".ToCharArray();
+        public static readonly string specialZero = "()";
+
+        public static int CountZero(string s)
+        {
+            var zeroCount = 0;
+            if (s.Contains(specialZero)) zeroCount += 1;
+            foreach (var symbol in s)
+            {
+                if (singleZeros.Contains(symbol))
+                    zeroCount += 1;
+                if (doubleZeros.Contains(symbol))
+                    zeroCount += 2;
+            }
+            return zeroCount;
+        }
+    }
+}
