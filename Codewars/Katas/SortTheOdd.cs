388df2de8d57f25846dba2f4d545c36f8a4ae625
Tomek Giela <tomasz.giela@hotmail.com>
2021-04-18 13:24:08 +0200

    Add SortTheOdd Kata

---
+++ Codewars/Katas/SortTheOdd.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Katas
+{
+    public class SortTheOdd
+    {
+        public static int[] SortArray(int[] array)
+        {
+            var onlyOddNumbers = new List<int>();
+            var evenNumbersWithPosition = new Dictionary<int, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    evenNumbersWithPosition.Add(i, array[i]);
+                else
+                    onlyOddNumbers.Add(array[i]);
+            }
+
+            var sortedOddNumbers = onlyOddNumbers.OrderBy(num => num).ToList();
+            foreach (var number in evenNumbersWithPosition)
+            {
+                sortedOddNumbers.Insert(number.Key, number.Value);
+            }
+            return sortedOddNumbers.ToArray();
+        }
+    }
+}
---
+++ XUnitKatasTests/SortTheOddShould.cs
@@ -0,0 +1,17 @@
+﻿using Codewars.Katas;
+using Xunit;
+
+namespace XUnitKatasTests
+{
+    public class SortTheOddShould
+    {
+        [Theory]
+        [InlineData(new int[] { 7, 1 }, new int[] { 1, 7 })]
+        [InlineData(new int[] { 7, 4, 1 }, new int[] { 1, 4, 7 })]
+        [InlineData(new int[] { 5, 3, 1, 8, 0 }, new int[] { 1, 3, 5, 8, 0 })]
+        public void ShouldSortOnlyOddNumbers(int[] arrayToSort, int[] expectedArray)
+        {
+            Assert.Equal(expectedArray, SortTheOdd.SortArray(arrayToSort));
+        }
+    }
+}
