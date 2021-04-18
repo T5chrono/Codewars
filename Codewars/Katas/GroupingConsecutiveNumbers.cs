7fee9e2360129a1f16c35a9f1033f12306c8cbce
Tomek Giela <tomasz.giela@hotmail.com>
2021-04-18 16:37:38 +0200

    Add GroupingConsecutiveNumbers Kata

---
+++ Codewars/Katas/GroupingConsecutiveNumbers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Katas
+{
+    public class GroupingConsecutiveNumbers
+    {
+        public static string GenerateGroupings(int[] input)
+        {
+            if (input.Length == 1) return $"{input[0]}";
+
+            var sortedInput = input.OrderBy(n => n).ToArray();
+
+            var firstDigit = sortedInput[0];
+            var lastDigit = 0;
+
+            var listOfRanges = new List<string>();
+            for (int i = 0; i < sortedInput.Length - 1; i++)
+            {
+                if (sortedInput[i + 1] - sortedInput[i] <= 1)
+                {
+                    lastDigit = sortedInput[i + 1];
+                    if (sortedInput.Length == i + 2)
+                        AddRange(sortedInput, ref firstDigit, ref lastDigit, listOfRanges, i);
+                }
+                else
+                {
+                    if (lastDigit != 0)
+                    {
+                        AddRange(sortedInput, ref firstDigit, ref lastDigit, listOfRanges, i);
+                        if (sortedInput.Length == i + 2)
+                            listOfRanges.Add($"{sortedInput[i + 1]}");
+                    }
+                    else
+                    {
+                        listOfRanges.Add($"{sortedInput[i]}");
+                        firstDigit = sortedInput[i + 1];
+                        if (sortedInput.Length == i + 2)
+                            listOfRanges.Add($"{sortedInput[i+1]}");
+                    }
+                }
+            }
+            
+            return String.Join(", ", listOfRanges); 
+        }
+
+        private static void AddRange(int[] sortedInput, ref int firstDigit, ref int lastDigit, List<string> listOfRanges, int i)
+        {
+            listOfRanges.Add($"{firstDigit}-{lastDigit}");
+            firstDigit = sortedInput[i + 1];
+            lastDigit = 0;
+        }
+    }
+}
---
+++ XUnitKatasTests/GroupingConsecutiveNumbersShould.cs
@@ -0,0 +1,19 @@
+﻿using Codewars.Katas;
+using Xunit;
+
+namespace XUnitKatasTests
+{
+    public class GroupingConsecutiveNumbersShould
+    {
+        [Theory]
+        [InlineData(new int[] { 1 }, "1")]
+        [InlineData(new int[] { 1, 3 }, "1, 3")]
+        [InlineData(new int[] { 1, 2, 3 }, "1-3")]
+        [InlineData(new int[] { 1, 2, 3, 5 }, "1-3, 5")]
+        [InlineData(new int[] { 1, 2, 3, 5, 9, 10}, "1-3, 5, 9-10")]
+        public void ReturnProperConcatenatedString(int[] array, string expectedString)
+        {
+            Assert.Equal(expectedString, GroupingConsecutiveNumbers.GenerateGroupings(array));
+        }
+    }
+}
