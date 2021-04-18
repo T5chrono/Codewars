37dc021e6be747e64bcebee9e5ecc6049f3c4787
Tomek Giela <tomasz.giela@hotmail.com>
2021-04-18 12:05:46 +0200

    Add ClimbersRankings Kata

---
+++ Codewars/Katas/ClimbersRankings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Katas
+{
+    public class ClimbersRankings
+    {
+        public static Dictionary<string, int> GetRankings(Dictionary<string, IEnumerable<int>> pointsByClimber)
+        {
+            // todo:   should return rankings by climber, where best 6 results for climber count... 
+            // todo:   return them in descending order (the one with most points is first)
+            // todo:   returned collection format: string Name Surname, int Sum of counting points
+            var rankings = new Dictionary<string, int>();
+            foreach (var entry in pointsByClimber)
+            {
+                var top6PointsSum = entry.Value.OrderByDescending(p => p).Take(6).Sum();
+                rankings.Add(entry.Key, top6PointsSum);
+            }
+            var sortedRankings = rankings.OrderByDescending(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            return sortedRankings;
+        }
+    }
+}
---
+++ XUnitKatasTests/ClimbersRankingsShould.cs
@@ -0,0 +1,46 @@
+﻿using Codewars.Katas;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace XUnitKatasTests
+{
+    public class ClimbersRankingsShould
+    {
+        public Dictionary<string, IEnumerable<int>> Rankings { get; set; }
+
+        public ClimbersRankingsShould()
+        {
+            Rankings = new Dictionary<string, IEnumerable<int>>
+            {
+                {"SKOFIC Domen", new[] {55, 100, 100, 25, 100, 51, 80}}
+            };
+        }
+
+        [Fact]
+        public void ReturnProperSum()
+        {
+            var result = ClimbersRankings.GetRankings(Rankings);
+            Assert.Equal(486, result["SKOFIC Domen"]);
+        }
+
+        [Fact]
+        public void ReturnProperSumEvenForLessPointed()
+        {
+            Rankings = new Dictionary<string, IEnumerable<int>>
+            {
+                {"SKOFIC Domen", new[] {100, 100, 25}}
+            };
+            var result = ClimbersRankings.GetRankings(Rankings);
+            Assert.Equal(225, result["SKOFIC Domen"]);
+        }
+
+        [Fact]
+        public void ReturnProperlySortedClimbers()
+        {
+            Rankings.Add("WorseSKOFIC Domen", new[] { 100, 100, 26 });
+            var result = ClimbersRankings.GetRankings(Rankings).OrderBy(pair => pair.Key).First();
+            Assert.Equal(486, result.Value);
+        }
+    }
+}
