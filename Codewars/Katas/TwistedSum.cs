2439078e539ab002703aeceb03529478a8ddaeb0
Tomek Giela <tomasz.giela@hotmail.com>
2021-04-20 15:15:44 +0200

    Add TwistedSum Kata

---
+++ Codewars/Katas/TwistedSum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewars.Katas
+{
+    public class TwistedSum
+    {
+        public static long Solution(long n)
+        {
+            var totalTwistedSum = 0;
+            for (var i = 0; i <= n; i++)
+            {
+                var numberAsString = i.ToString();
+                totalTwistedSum += numberAsString.Sum(digit => Convert.ToInt32(digit.ToString()));
+            }
+
+            return totalTwistedSum;
+        }
+    }
+}
---
+++ NUnitKatasTests/TwistedSumShould.cs
@@ -0,0 +1,30 @@
+﻿using Codewars.Katas;
+using NUnit.Framework;
+
+namespace NUnitKatasTests
+{
+    [TestFixture]
+    public class TwistedSumShould
+    {
+        [Test]
+        public void When03_Then6()
+        {
+            Assert.AreEqual(6, TwistedSum.Solution(3));
+        }
+        [Test]
+        public void When10_Then46()
+        {
+            Assert.AreEqual(46, TwistedSum.Solution(10));
+        }
+        [Test]
+        public void When11_Then48()
+        {
+            Assert.AreEqual(48, TwistedSum.Solution(11));
+        }
+        [Test]
+        public void When12_Then51()
+        {
+            Assert.AreEqual(51, TwistedSum.Solution(12));
+        }
+    }
+}
